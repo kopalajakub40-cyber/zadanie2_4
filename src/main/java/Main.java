5416a07af4f461748d678bfcb5a9c78111deca88
kopalajakub40-cyber <kopalajakub40@gmail.com>
2025-10-26 18:09:22 +0000

    zad2_3

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,16 @@
+import java.util.Scanner;
+
 public class Main{
   public static void main(String[] args){
-    int liczba=42;
-    System.out.println("Liczba: "+liczba);
+    Scanner liczenie = new Scanner(System.in);
+
+    System.out.println("Podaj 1 liczbę: ");
+    double a = liczenie.nextDouble();
+
+    System.out.println("Podaj 2 liczbę: ");
+    double b = liczenie.nextDouble();
+
+    double suma = a+b;
+    System.out.println("Suma wynosi: " + suma);
   }
 }

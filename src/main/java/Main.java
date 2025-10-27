d5921a8faa4eb7ce9953e8c0b8b865f17fc75d82
kopalajakub40-cyber <kopalajakub40@gmail.com>
2025-10-27 08:13:48 +0000

    zad2_5

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,40 +1,10 @@
-import java.util.Scanner;
-
 public class Main{
     public static void main(String[] args){
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Wybierz dzialanie");
-        System.out.println("1 - suma");
-        System.out.println("2 - roznica");
-        System.out.println("3 - iloczyn");
-        System.out.println("4 - iloraz");
-        int dzialanie = scan.nextInt();
-      
-       System.out.println("Podaj pierwsza liczbe");
-       double a = scan.nextDouble();
-       System.out.println("Podaj druga liczbe");
-       double b = scan.nextDouble();
-      
-      switch(dzialanie){
-          case 1:
-          System.out.println("Wynik::"+(a + b));
-          break;
-        case 2:
-          System.out.println(a - b);
-          break;
-        case 3:
-          System.out.println(a * b);
-          break;
-        case 4:
-          if (b==0){
-            System.out.println("Nie dzielimy przez zero");
-          }
-          else{
-            System.out.println(a / b);
-          }
-          break;
-        default:
-          System.out.println("Zły wybór");
+      System.out.println("Liczby parzyste z zakredu od 1 do 100");
+      for(int i=0; i <= 100; i++){
+        if(i%2==0){
+          System.out.println(i);
+        }
       }
     }
 }

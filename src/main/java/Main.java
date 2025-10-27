911f2f2ef79fad3d69cb3f7bc995b88cae731628
kopalajakub40-cyber <kopalajakub40@gmail.com>
2025-10-27 08:06:52 +0000

    zad2_4

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,16 +1,40 @@
 import java.util.Scanner;
 
 public class Main{
-  public static void main(String[] args){
-    Scanner liczenie = new Scanner(System.in);
-
-    System.out.println("Podaj 1 liczbę: ");
-    double a = liczenie.nextDouble();
-
-    System.out.println("Podaj 2 liczbę: ");
-    double b = liczenie.nextDouble();
-
-    double suma = a+b;
-    System.out.println("Suma wynosi: " + suma);
-  }
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Wybierz dzialanie");
+        System.out.println("1 - suma");
+        System.out.println("2 - roznica");
+        System.out.println("3 - iloczyn");
+        System.out.println("4 - iloraz");
+        int dzialanie = scan.nextInt();
+      
+       System.out.println("Podaj pierwsza liczbe");
+       double a = scan.nextDouble();
+       System.out.println("Podaj druga liczbe");
+       double b = scan.nextDouble();
+      
+      switch(dzialanie){
+          case 1:
+          System.out.println("Wynik::"+(a + b));
+          break;
+        case 2:
+          System.out.println(a - b);
+          break;
+        case 3:
+          System.out.println(a * b);
+          break;
+        case 4:
+          if (b==0){
+            System.out.println("Nie dzielimy przez zero");
+          }
+          else{
+            System.out.println(a / b);
+          }
+          break;
+        default:
+          System.out.println("Zły wybór");
+      }
+    }
 }
